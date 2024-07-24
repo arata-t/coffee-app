b48b8566737c371b52f998a07ac4054635aba9f9
arata-t <76896195+arata-t@users.noreply.github.com>
2024-07-24 22:17:18 +0900

    feat: #37 x-server対応

--- coffee-app/src/main.js
+++ coffee-app/src/main.js
@@ -1,4 +1,5 @@
-import '@fortawesome/fontawesome-free/css/all.css' 
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@mdi/font/css/materialdesignicons.css';
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
--- coffee-app/src/plugins/vuetify.js
+++ coffee-app/src/plugins/vuetify.js
@@ -1,7 +1,11 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
+import '@mdi/font/css/materialdesignicons.css'; // 追加
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    icons: {
+        iconfont: 'mdi', // この行を追加
+    },
 });
